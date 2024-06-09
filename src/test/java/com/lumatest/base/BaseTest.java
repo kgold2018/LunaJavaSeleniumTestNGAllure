8a7897032211b08ea5c240876dcf34b96c01da18
Lara G <38481415+kgold2018@users.noreply.github.com>
2024-06-09 14:01:53 -0700

    Test9 (#15)
    * addDornyReport
    
    * modifiedmAVENyML
    
    * add description to tests
    
    * add testing.xml and firefox browser
    
    * testng.xml
    
    ---------
    
    Co-authored-by: Klavdia <kgold94@yahoo.com>

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -5,28 +5,55 @@ import com.lumatest.utils.DriverUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest {
     private WebDriver driver;
-
+   // private final String browser = "chrome";
+//   private final String browser = "firefox";
     @BeforeSuite
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
     }
 
+    @Parameters("browser")
+  //  String browser
     @BeforeMethod
-    protected void setupDriver() {
-        this.driver = DriverUtils.createChromeDriver(getDriver());
+    protected void setupDriver(String browser) {
+        Reporter.log("--------------------------------------------------------------------------", true);
+        this.driver = DriverUtils.createDriver(browser, this.driver);
+
+        if(getDriver() == null) {
+            Reporter.log("ERROR: Unknown parameter 'browser' - '" + browser + "'.", true);
+
+            System.exit(1);
+        }
+
+        Reporter.log("INFO: " + browser.toUpperCase() + " driver created.", true);
+//        if(this.browser.equals("chrome")) {
+//            this.driver = DriverUtils.createChromeDriver(getDriver());
+//            Reporter.log("INFO: Chrome browser created.", true);
+//        } else {
+//            Reporter.log("ERROR: Unknown parameter 'browser' - '" + this.browser + "'.", true);
+//            System.exit(1);
+//        }
     }
 
+    @Parameters("browser")
     @AfterMethod(alwaysRun = true)
-    protected void tearDown() {
+    protected void tearDown(String browser) {
         if (this.driver != null) {
             getDriver().quit();
+            Reporter.log("INFO: " + browser.toUpperCase() + " driver closed.", true);
+
             this.driver = null;
+        } else {
+            Reporter.log("INFO: Driver is null", true);
         }
     }
 
--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -1,13 +1,18 @@
 package com.lumatest.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Reporter;
 
 import java.util.Map;
 
 public class DriverUtils {
     private static final ChromeOptions chromeOptions;
+    private static final FirefoxOptions firefoxOptions;
 
     static {
         chromeOptions = new ChromeOptions();
@@ -21,10 +26,21 @@ public class DriverUtils {
         chromeOptions.addArguments("--disable-web-security");
         chromeOptions.addArguments("--allow-running-insecure-content");
         chromeOptions.addArguments("--ignore-certificate-errors");
+
+        firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--incognito");
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--window-size=1920,1080");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
+        firefoxOptions.addArguments("--ignore-certificate-errors");
     }
 
     //for Luma
-    public static WebDriver createChromeDriver(WebDriver driver) {
+    private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver != null) {
             driver.quit();
         }
@@ -36,6 +52,44 @@ public class DriverUtils {
         );
 
 
-        return chromeDriver ;
+        return chromeDriver;
+    }
+
+    //    public static WebDriver createDriver(String browser, WebDriver driver ){
+//        if(browser.equals("chrome")) {
+//            driver = DriverUtils.createChromeDriver(driver);
+//           // Reporter.log("INFO: Chrome driver created.", true);
+//
+//            return driver;
+//        } else {
+//            //Reporter.log("ERROR: Unknown parameter 'browser' - '" + browser + "'.", true);
+//            return null;
+//        }
+//    }
+    public static WebDriver createFireFoxDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
+//        firefoxDriver.executeCdpCommand("Network.enable", Map.of());
+//        firefoxDriver.executeCdpCommand(
+//                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
+//        );
+
+        return firefoxDriver;
+    }
+
+    public static WebDriver createDriver(String browser, WebDriver driver) {
+        switch (browser) {
+            case "chrome" -> {
+                return createChromeDriver(driver);
+            }
+            case "firefox" -> {
+                return createFireFoxDriver(driver);
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
